2214fd76fc7941f57bdc1c2b65a69be74a78efe6
unknown <duongn11@outlook.com>
2019-10-14 01:33:04 +0700

    module 2

---
+++ Module2/src/Session1_JavaOverview/Practice/P1_showTimeNow.java
@@ -0,0 +1,10 @@
+package Session1_JavaOverview.Practice;
+import java.util.Date;
+
+public class P1_showTimeNow {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Time now is: " + now);
+    }
+}
+
---
+++ Module2/src/Session1_JavaOverview/Practice/P5_.java
@@ -0,0 +1,5 @@
+package Session1_JavaOverview.Practice;
+
+public class P5_ {
+
+}
