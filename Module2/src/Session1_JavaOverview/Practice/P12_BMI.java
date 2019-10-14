67cdec0f11be0f83cd0d09c1df3d779d361a6b60
nhiduong11 <duongn11@outlook.com>
2019-10-14 14:40:10 +0700

    [*Thực hành] Tính chỉ số cân nặng của cơ thể

---
+++ Module2/src/Session1_JavaOverview/Practice/P12_BMI.java
@@ -0,0 +1,28 @@
+package Session1_JavaOverview.Practice;
+
+import java.util.Scanner;
+
+public class P12_BMI {
+    public static void main(String[] args) {
+        double weight;
+        double height;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your weight: ");
+        weight = input.nextDouble();
+        System.out.print("Enter your height: ");
+        height = input.nextDouble();
+
+        double bmi;
+        bmi = weight / Math.pow(height , 2);
+
+        if (bmi < 18)
+            System.out.printf("%-20.2f%s", bmi, "Underweight");
+        else if (bmi < 25.0)
+            System.out.printf("%-20.2f%s", bmi, "Normal");
+        else if (bmi < 30.0)
+            System.out.printf("%-20.2f%s", bmi, "Overweight");
+        else
+            System.out.printf("%-20.2f%s", bmi, "Obese");
+        System.out.printf("%-20s%s", "bmi", "Interpretation\n");
+    }
+}
