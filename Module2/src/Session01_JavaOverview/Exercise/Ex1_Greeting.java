341cd211ba6d824af84c11a85944c649da59fde9
nhiduong11 <duongn11@outlook.com>
2019-10-18 15:02:01 +0700

    [Bài tập] Hiển thị lời chào

---
+++ Module2/src/Session01_JavaOverview/Exercise/Ex1_Greeting.java
@@ -0,0 +1,14 @@
+package Session01_JavaOverview.Exercise;
+
+import java.util.Scanner;
+
+public class Ex1_Greeting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name);
+    }
+}
+
