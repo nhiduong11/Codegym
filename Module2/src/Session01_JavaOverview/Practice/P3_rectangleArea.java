8e582cbc87724614d6ee015e5ca622c415e0391c
nhiduong11 <duongn11@outlook.com>
2019-10-18 15:04:13 +0700

    [Thực hành] Khai báo và sử dụng biến

---
+++ Module2/src/Session01_JavaOverview/Practice/P3_rectangleArea.java
@@ -0,0 +1,22 @@
+package Session01_JavaOverview.Practice;
+
+import java.util.Scanner;
+
+public class P3_rectangleArea {
+    public static void main(String[] args) {
+        float width;
+        float height;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter width: ");
+        width = scanner.nextFloat();
+
+        System.out.println("Enter height: ");
+        height = scanner.nextFloat();
+
+        //Rectangle Area
+        float area = width * height;
+        System.out.println("Area is: " + area);
+    }
+}
+
